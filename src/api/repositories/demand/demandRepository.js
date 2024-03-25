500c3610e4c8eb24eed88c07d2309fe7b939ba09
Elenilson Monteiro <monteir.elenilson@gmail.com>
2024-03-25 12:04:06 -0300

    adding client and dish to demand

--- src/api/repositories/demand/demandRepository.js
+++ src/api/repositories/demand/demandRepository.js
@@ -1,5 +1,7 @@
 const Demand = require("../../models/demand");
 const itemRepository = require("../item/itemRepository");
+const userRepository = require("../user/userRepository");
+const dishRepository = require("../dish/dishRepository");
 
 const findAll = async () => {
   const demands = await Demand.findAll();
@@ -17,15 +19,24 @@ const findById = async (id) => {
 };
 
 const addItem = async (itens, demand) => {
-  itens.array.forEach(async (element) => {
+  itens.forEach(async (element) => {
     const item = await itemRepository.create(element);
-    await demand.addItem(item);
+    await demand.addItens(item);
+
+    const dish = await dishRepository.findById(element.dish.id);
+    await dish.addItens(item);
   });
+};
+
+const addClient = async (userId, demand) => {
+  const user = await userRepository.findById(userId);
+  await user.addDemands(demand);
 };
 
 const create = async (demand) => {
   const { itens, ..._demand } = demand;
   const _demand_ = await Demand.create(_demand);
+  await addClient(demand.user.id, _demand_);
   await addItem(itens, _demand_);
   return _demand_;
 };
