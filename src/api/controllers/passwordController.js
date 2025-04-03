794d23b8f300178e8e753b7495fa2489d066d66c
Elenilson Monteiro <monteir.elenilson@gmail.com>
2025-04-03 20:20:32 -0300

    Adjust encoded new password in the reset password process

--- src/api/controllers/passwordController.js
+++ src/api/controllers/passwordController.js
@@ -1,6 +1,6 @@
-import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/user.js";
+import { encrypt } from "../../utils/encrypt.js";
 
 export const resetPassword = async (req, res) => {
   const { token, newPassword } = req.body;
@@ -13,7 +13,7 @@ export const resetPassword = async (req, res) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    const hashedPassword = encrypt(newPassword);
 
     const [updatedRows] = await User.update(
       { password: hashedPassword },
@@ -24,7 +24,7 @@ export const resetPassword = async (req, res) => {
       return res.status(404).json({ error: "User not found." });
     }
 
-    return res.json({ message: "Password reset successfully." });
+    return res.status(200).json({ message: "Password reset successfully." });
   } catch (error) {
     return res.status(400).json({ error: "Invalid or expired token." });
   }
