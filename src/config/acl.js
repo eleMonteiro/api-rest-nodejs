e0b3c0302221cba366aea4c6941eb23de8d02287
Elenilson Monteiro <monteir.elenilson@gmail.com>
2025-04-05 20:04:39 -0300

    adjusts permissions

--- src/config/acl.js
+++ src/config/acl.js
@@ -1,11 +1,19 @@
 export const config = {
-  baseUrl: "/",
+  baseUrl: "/api/v1/",
   filename: "nacl.json",
   roleSearchPath: "user.role",
+  decodedObjectName: "user",
   path: "./",
+  denyCallback: (res) => {
+    return res.status(403).json({
+      status: "Access Denied",
+      message: "You are not authorized to access this resource",
+    });
+  },
 };
 
 export const responseObject = {
   status: "Access Denied",
   message: "You are not authorized to access this resource",
+  code: 403,
 };
